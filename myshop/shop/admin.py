7a98b76639e894572277c331ad2d0b67d9204281
rookiesuri <rookiesuri@users.noreply.github.com>
2018-03-06 16:38:34 +0530

    creating admin module

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Category,Product
+
+
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    prepopulated_fields={'slug':('name',)}
+admin.site.register(Category,CategoryAdmin)
